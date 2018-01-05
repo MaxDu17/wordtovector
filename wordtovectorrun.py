20c1bc8c0be5dd597491cc6632a66a1b6ae78536
Max Du <maxdubusiness@gmail.com>
2018-01-05 17:31:34 -0500

    w

--- wordtovectorrun.py
+++ wordtovectorrun.py
@@ -19,17 +19,17 @@ center_index, target_index = initialize_vars(hyp)
 embed = initialize_embed(hyp, center_index)
 loss, optimizer = initialize_loss_and_optimizer(hyp,target_index,embed)
 
- with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        total_loss = 0.0  # we use this to calculate the average loss in the last SKIP_STEP steps
-        writer = tf.summary.FileWriter('./graphs/no_frills/', sess.graph)
-        for index in range(hyp.NUM_TRAIN_STEPS):
-            batch = next(batch_gen)
-            loss_batch, _ = sess.run([loss,optimizer], feed_dict = {center_index: batch[0], target_index: batch[1]})# the "_" just acts like a dump ground for "optimizer"
-            total_loss = total_loss + loss_batch
-            if (index + 1) % hyp.SKIP_STEP == 0:
-                print('Average loss at step {}: {:5.1f}'.format(index, total_loss / hyp.SKIP_STEP))
-                total_loss = 0.0
-        writer.close()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    total_loss = 0.0  # we use this to calculate the average loss in the last SKIP_STEP steps
+    writer = tf.summary.FileWriter('./graphs/no_frills/', sess.graph)
+    for index in range(hyp.NUM_TRAIN_STEPS):
+        batch = next(batch_gen)
+        loss_batch, _ = sess.run([loss,optimizer], feed_dict = {center_index: batch[0], target_index: batch[1]})# the "_" just acts like a dump ground for "optimizer"
+        total_loss = total_loss + loss_batch
+        if (index + 1) % hyp.SKIP_STEP == 0:
+            print('Average loss at step {}: {:5.1f}'.format(index, total_loss / hyp.SKIP_STEP))
+            total_loss = 0.0
+    writer.close()
 
 
