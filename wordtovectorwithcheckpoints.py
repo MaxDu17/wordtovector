195f8c659e83cf26b21355460f9a240fb752faa8
Max Du <maxdubusiness@gmail.com>
2018-01-07 16:06:37 -0500

    a

--- wordtovectorwithcheckpoints.py
+++ wordtovectorwithcheckpoints.py
@@ -54,7 +54,7 @@ def word2vec(batch_gen):
         for index in range(NUM_TRAIN_STEPS):
             batch = next(batch_gen)
             loss_batch, _, summary = sess.run([loss,optimizer,summary_op], feed_dict = {center_index: batch[0], target_index: batch[1]})# the "_" just acts like a dump ground for "optimizer"
-            writer.add_summary(summary,global_step = global_step)
+            writer.add_summary(summary,global_step = index)
             total_loss = total_loss + loss_batch
             if (index + 1) % SKIP_STEP == 0:
                 print('Average loss at step {}: {:5.1f}'.format(index, total_loss / SKIP_STEP))
